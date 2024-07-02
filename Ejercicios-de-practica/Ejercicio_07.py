560141b92e8780f474efacf57f4edb9623760572
Scar 😺 <115835282+Meow23464@users.noreply.github.com>
2024-07-02 12:41:11 -0400

    Add files via upload

---
+++ Ejercicios-de-practica/Ejercicio_07.py
@@ -0,0 +1,10 @@
+base = float(input("Introduce la base del rectángulo: "))
+altura = float(input("Introduce la altura del rectángulo: "))
+
+
+area = base * altura
+perimetro = 2 * (base + altura)
+
+
+print("Área del rectángulo:", area)
+print("Perímetro del rectángulo:", perimetro)
